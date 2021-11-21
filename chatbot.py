14c0b5d4fa91bc367cb8e65dce3024f114b60172
Rabii <rabi3jouira@gmail.com>
2021-11-21 19:30:18 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -7,3 +7,10 @@ import string
 #Step 2 : Data reading
 f=open('chatbot.txt','r',errors='ignore')
 raw=f.read()
+
+#Step 3.1 : Preprocessing raw text (tokenization)
+raw=raw.lower()
+nltk.download('punkt')
+nltk.download('wordnet')
+sent_tokens=nltk.sent_tokenize(raw)
+word_tokens=nltk.word_tokenize(raw)
