be4e8b0e2328507a5e12754d4fd868e9c562e2f6
Rabii <rabi3jouira@gmail.com>
2021-11-21 19:30:56 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -14,3 +14,14 @@ nltk.download('punkt')
 nltk.download('wordnet')
 sent_tokens=nltk.sent_tokenize(raw)
 word_tokens=nltk.word_tokenize(raw)
+
+#Step 3.2 : Preprocessing raw text (lemmatization)
+lemmer=nltk.stem.WordNetLemmatizer()
+def LemTokens(tokens):
+  return [lemmer.lemmatize(token) for token in tokens]
+
+remove_punct_dict=dict((ord(punct),None) for punct in string.punctuation)
+
+def LemNormalize(text):
+  return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
